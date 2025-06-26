68d6214e9b148044c8a9384d26fde87e9bf4b4a5
Jazz <99632268+jasonmrcllino@users.noreply.github.com>
2025-06-27 00:16:41 +0700

    first init github

---
+++ src/jason_pekanuas/Jason_PekanUAS.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jason_pekanuas;
+
+/**
+ *
+ * @author Jazz
+ */
+public class Jason_PekanUAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
